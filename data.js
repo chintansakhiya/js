0ffce42a69c2a3f4128934a3cccb6c122c02435b
chintan <chintan.sakhiya@improwised.com>
2023-04-11 10:07:57 +0530

    minore change

--- data.js
+++ data.js
@@ -1,40 +1,40 @@
-const users=["chintansakhiya","ami-kalola","bhoomiz01","VatsaL","sgdghdtrh","htsehtshts","chintansakhiya123"]
-const loading =  require('loading-cli');
-const load = loading("loading text!!")
-let result=[]
-let vuser=0
-let xuser=0
- 
+const users = ["chintansakhiya", "ami-kalola", "bhoomiz01", "VatsaL", "sgdghdtrh", "htsehtshts", "chintansakhiya"]
+const loading = require('loading-cli');
+const load = loading("loading details!!")
+let result = []
+let vUser = 0
+let inUser = 0
+
 async function getData() {
     const start = Date.now();
-  for ((user) in users){
-    
-    let temp = await fetch(`https://api.github.com/users/${users[user]}`)
-    let res=await temp.json()
-    result.push(res) 
-  }
-  result.forEach(res=>{
- 
-    if(res.id==undefined){
-     
-      xuser++
-    }else{
-      vuser++
+    for ((user) in users) {
+
+        let temp = await fetch(`https://api.github.com/users/${users[user]}`)
+        let res = await temp.json()
+        result.push(res)
     }
-  })
+    result.forEach(res => {
+
+        if (res.id == undefined) {
+
+            inUser++
+        } else {
+            vUser++
+        }
+    })
     const end = Date.now();
     load.stop()
 
     const millis = end - start
     console.log("time= " + millis + " ms")
-  console.log("valid:"+vuser+" invalid:"+xuser)
-     
+    console.log("valid:" + vUser + " invalid:" + inUser)
+
 }
 
 
-function x() {
+function startProgram() {
     load.start()
-     getData()
-  
+    getData()
+
 }
-x()
+startProgram()
