7eeb31f8d6df32b47e8641e33c0cdc7b574c09b0
chintan <chintan.sakhiya@improwised.com>
2023-04-11 09:55:57 +0530

    task completed

--- data.js
+++ data.js
@@ -1,49 +1,40 @@
-// function twirlTimer(bool) {
-//     let myInterval
-//     if (bool === true) {
-//         let P = ["\\", "|", "/", "-"];
-//         let x = 0;
-//         myInterval=setInterval(function() {
-//             process.stdout.write("\r" + P[x++] + "loading");
-//             x &= 3;
-//         }, 250);
-//     }else{
-//         console.log("fs")
-//         clearInterval(myInterval); 
-//     }
-
-// };
-const users=["chintansakhiya","ami-kalola","bhoomiz01","VatsaL","sgdghdtrh","htsehtshts","chintansakhiya"]
+const users=["chintansakhiya","ami-kalola","bhoomiz01","VatsaL","sgdghdtrh","htsehtshts","chintansakhiya123"]
 const loading =  require('loading-cli');
 const load = loading("loading text!!")
-
+let result=[]
+let vuser=0
+let xuser=0
+ 
 async function getData() {
     const start = Date.now();
-    let details=users.map((user)=> fetch(`https://api.github.com/${user}`));
-    const end = Date.now();
+  for ((user) in users){
     
-    let detail=details.map(async (user)=>  console.log(user));
+    let temp = await fetch(`https://api.github.com/users/${users[user]}`)
+    let res=await temp.json()
+    result.push(res) 
+  }
+  result.forEach(res=>{
+ 
+    if(res.id==undefined){
+     
+      xuser++
+    }else{
+      vuser++
+    }
+  })
+    const end = Date.now();
+    load.stop()
 
     const millis = end - start
-console.log(detail)
-    
-
-    
-    // console.log("\ntotal users " + result[0].iddd)
-    console.log("time= " + millis / 1000000 + " s")
-
-
-    load.stop()
-    return Promise.resolve(true)
+    console.log("time= " + millis + " ms")
+  console.log("valid:"+vuser+" invalid:"+xuser)
+     
 }
 
 
 function x() {
     load.start()
-    y = getData()
-
-     
-
-
+     getData()
+  
 }
 x()
