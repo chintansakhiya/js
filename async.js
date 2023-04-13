330853d6b080ed9b5091c566c7f459fa80358ee8
chintan <chintan.sakhiya@improwised.com>
2023-04-13 14:30:32 +0530

    optimize code

--- async.js
+++ async.js
@@ -1,37 +1,46 @@
 // import fetch from "node-fetch"
 //get uuid 
-async function getUUID(){
-    let res=await fetch("https://httpbin.org/uuid")
-    let data=await res.json()
+async function getUUID() {
+    let res = await fetch("https://httpbin.org/uuid")
+    let data = await res.json()
     return data
-  }
-  
-  //delay
-  async function delayed(delay){
-    let res=await fetch(`https://httpbin.org/delay/${delay}`)
-  }
-  
-  //post data
-  async function postBody(body){
-    let res=await fetch(`https://httpbin.org/anything/}`,{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body:JSON.stringify(body)
+}
+
+//delay
+async function delayed(delay) {
+    let res = await fetch(`https://httpbin.org/delay/${delay}`)
+    
+}
+
+//post data
+async function postBody(body) {
+    let res = await fetch(`https://httpbin.org/anything/}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
     })
-    let data=await res.json()
+    let data = await res.json()
     return data
-  }
-  
-  let startTime=new Date().getTime()
-  
-  let result=getUUID()
-  result.then((response)=>{
-    delayed(2).then(()=>{
-      postBody(response).then((res)=>{
-        let endTime=new Date().getTime()
-        console.log(res.data, (endTime-startTime)/1000+"s")
-      })
-    })
-  })
+}
+
+
+//   let result=getUUID()
+//   result.then((response)=>{
+//     delayed(2)
+//     postBody(response).then((res)=>{
+//         let endTime=new Date().getTime()
+//         console.log(res.data, (endTime-startTime)/1000+"s")
+//       })
+//   })
+
+async function run() {
+    let uuid = getUUID()
+    delayed(2)
+    let data =await postBody(await uuid)
+    let endTime=new Date().getTime()
+    console.log(data.data,(endTime-startTime)/1000+"s");
+}
+let startTime = new Date().getTime()
+run()
